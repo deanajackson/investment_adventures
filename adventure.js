19a6dcb8951c597a1cad300814785e137dddd3bf
Deana <demaisja@gmail.com>
2021-12-04 14:10:53 -0800

    Whole message fades in buttons don't yet

--- adventure.js
+++ adventure.js
@@ -1,1 +1,28 @@
-const text = document.querySelector(".mentor-space");
+
+    const text = document.querySelector("#msg");
+    const strText = text.textContent;
+    const splitText = strText.split("");
+    text.textContent = "";
+
+    for(let i=0; i < splitText.length; i++){
+        text.innerHTML += "<span>" + splitText[i] + "</span>";   
+     }
+console.log(splitText);
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
